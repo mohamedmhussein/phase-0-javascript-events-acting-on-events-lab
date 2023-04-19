7184f4d0af1e7807153f4081cd65645c88036fe5
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-04-18 22:53:28 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4"
+
+// document.addEventListener("keydown", function (e) {
+//     console.log(e)});
+
+// Move left
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 2}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+////////////////////////////////////////////////////
+// Move right
+document.addEventListener("keydown", e => {
+    if (e.key === "ArrowRight"){
+        moveDodgerRight()
+    }
+})
+
+function moveDodgerRight(){
+    const leftNumber = dodger.style.left.replace("px", "")
+    const left = parseInt(leftNumber, 10)
+    if (left < 380){ dodger.style.left = `${left + 2}px`;}
+}
+
+
